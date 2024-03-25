9880cf6c656c364428baf5f2d652fc5640120594
nazaninh0 <nazanin.heydari@learnet.se>
2024-03-25 10:35:07 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("ange ditt namn ");
         String name = scan.nextLine();
         System.out.println("ange ditt födesle datum");
-        int birthdate = scan.nextInt(); 
+        int birthdate = scan.nextInt();
 
     }
 }
