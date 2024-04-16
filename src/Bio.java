e7a3bb341c00ed9ecff3d715ab575bef0001d6e4
nazaninh0 <nazanin.heydari@learnet.se>
2024-04-16 14:55:53 +0200

    Initial commit

--- src/Bio.java
+++ src/Bio.java
@@ -1,25 +1,47 @@
-public class Bio {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Bio {
         private String title;
         //private int birthdate;
         private int movieId;
         private String genre;
+        private List<Integer> bookedSeats;
+        private int hallId;
+        private String name;
+        private int capacity;
 
-    public Bio ( int movieId , String title, String genre)
-    {
-        this.movieId = movieId;
-        this.title = title;
-        this. genre = genre;
-    }
-    public int getMovieId() {
-        return movieId;
-    }
+        public Bio(int movieId, String title, String genre) {
+            this.movieId = movieId;
+            this.title = title;
+            this.genre = genre;
+            this.bookedSeats = new ArrayList<>();
+        }
 
-    public String getTitle() {
-        return title;
+        public int getMovieId() {
+            return movieId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String genre() {
+            return genre;
+        }
+    public List<Integer> getBookedSeats() {
+        return bookedSeats;
     }
-    public String genre (){
-        return genre;
+    public boolean bookSeat(int seatNumber) {
+        if (!bookedSeats.contains(seatNumber)) {
+            bookedSeats.add(seatNumber);
+            return true; // Booking successful
+        }
+        return false; // Seat already booked
+    }
+    public boolean cancelSeatBooking(int seatNumber) {
+        return bookedSeats.remove(Integer.valueOf(seatNumber));
     }
 
 }
+
