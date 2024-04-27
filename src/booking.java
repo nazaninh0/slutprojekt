6248b74fff8e92fa0b9f797105612bc26dcd7fd9
nazaninh0 <nazanin.heydari@learnet.se>
2024-04-28 00:14:47 +0200

    Initial commit

--- src/booking.java
+++ src/booking.java
@@ -1,17 +1,34 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class booking {
     Customer owner;
-    //private List<Integer> bookedSeats;
+    private List<Integer> bookedSeats;
     private int Seat;
 
     public booking(int seat) {
         this.Seat = seat;
-
+        this.bookedSeats = new ArrayList<>();
     }
 
     public int getSeat() {
         return Seat;
+    }
+
+    public List<Integer> getBookedSeats() {
+        return bookedSeats;
+    }
+
+    public boolean bookSeat(int seatNumber) {
+        if (!bookedSeats.contains(seatNumber)) {
+            bookedSeats.add(seatNumber);
+            return true; // Booking successful
+        }
+        return false; // Seat already booked
+    }
+
+    public boolean cancelSeatBooking(int seatNumber) {
+        return bookedSeats.remove(Integer.valueOf(seatNumber));
     }
 
 }
