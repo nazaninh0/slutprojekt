55beede042a08b9925e1857d21c3c7e8e3feb2ff
nazaninh0 <nazanin.heydari@learnet.se>
2024-04-28 00:33:00 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,8 @@ public class Main {
         System.out.println("enter your age: ");
         int age = scan.nextInt();
         Customer customer = new Customer(name, age);
-        //Customer booking = new Customer(customer);
         System.out.println("customer name is " + customer.getName());
         System.out.println("customer age is " + customer.getAge());
-        //System.out.println("choose between one these movies (OBS !! write the number)  :");
         int movieId = scan.nextInt();
         Bio selectedMovie = null;
         while (true) {
@@ -42,7 +40,6 @@ public class Main {
                 System.out.println("sorry you are not allowed to watch this movie: " + selectedMovie.getTitle());
             }
 
-            //System.out.println("   1   2   3   4   5   6   7  \n 8   9   10   11   12   13   14 \n 15   16   17   18   19   20   21");
             System.out.println("choose a seat : ");
             int Seat = scan.nextInt();
             booking bookedSeat = new booking(Seat);
